0d3d4806e9db859e946efdc69c1c950ccbd80c7d
lid.guan <lid.guan@wayz.ai>
2020-11-17 16:22:25 +0800

    route的简单用法

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 
 import MyMap from './components/Amap';
+import GeoHash from './pages/GeoHash'
+import Login from './pages/Login'
+import S2 from './pages/S2'
+
 import { Button } from 'antd'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <MyMap/>,
+      <Router>
+        <Route path="/map" component={MyMap}></Route>
+        <Route path="/geohash" component={GeoHash}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/s2" component={S2}></Route>
+      </Router>
       <Button type="primary">Button</Button>
     </div>
   );
