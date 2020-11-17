86fc01286a2f176256556a339707c7d6197e235d
lid.guan <lid.guan@wayz.ai>
2020-11-17 11:59:46 +0800

    准备学习router

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <p>Geohash</p>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/S2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function S2() {
+  return (
+    <div>
+      <p>S2</p>
+    </div>
+  )
+}
+
+export default S2
