017ee300e7a4687e325837846dbdbd528c40bcc5
lid.guan <lid.guan@wayz.ai>
2020-11-18 14:36:09 +0800

    route 简单用法

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MyMap from './components/Amap';
 import GeoHash from './pages/GeoHash'
 import Login from './pages/Login'
 import S2 from './pages/S2'
+import UCenter from './pages/UCenter'
 
 import { Button } from 'antd'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
@@ -13,11 +14,19 @@ function App() {
   return (
     <div className="App">
       <Router>
+        {/* link的用法 */}
+        <Link to="/map">map</Link>
+
+
+        {/* route的用法 */}
         <Route path="/map" component={MyMap}></Route>
-        <Route path="/geohash" component={GeoHash}></Route>
+        <Route path="/geohash" exact={true} component={GeoHash}></Route>
         <Route path="/login" component={Login}></Route>
         <Route path="/s2" component={S2}></Route>
+
+        <Route path="/geohash/ucenter" component={UCenter}></Route>
       </Router>
+      
       <Button type="primary">Button</Button>
     </div>
   );
---
+++ src/pages/UCenter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UCenter extends Component {
+  render() {
+    return (
+      <div>
+        <p>Ucenter</p>
+      </div>
+    )
+  }
+}
